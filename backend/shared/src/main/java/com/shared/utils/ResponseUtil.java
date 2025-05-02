f215b528f9105a4838598f4d28620a5f5bd3171e
Vu Hieu <hieutrongvu281@gmail.com>
2025-05-02 20:45:43 +0700

    build base backend

---
+++ backend/shared/src/main/java/com/shared/utils/ResponseUtil.java
@@ -0,0 +1,17 @@
+package com.shared.utils;
+
+import com.shared.response.CommonResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class ResponseUtil {
+
+    public static ResponseEntity<CommonResponse<Object>> toSuccessCommonResponse(Object data) {
+        return new ResponseEntity<>(new CommonResponse<>(data), HttpStatus.OK);
+    }
+
+    public static ResponseEntity<CommonResponse<Object>> toInternalErrorCommonResponse() {
+        return new ResponseEntity<>(CommonResponse.internalError(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
