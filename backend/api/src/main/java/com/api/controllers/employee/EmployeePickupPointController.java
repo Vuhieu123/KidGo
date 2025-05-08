ebb45a3104b1dc936b465cb6b2f255cb5eb0386b
Vu Hieu <hieutrongvu281@gmail.com>
2025-05-08 23:55:11 +0700

    feat(api): create employee pickup point api

--- backend/api/src/main/java/com/api/controllers/employee/EmployeePickupPointController.java
+++ backend/api/src/main/java/com/api/controllers/employee/EmployeePickupPointController.java
@@ -1,2 +1,48 @@
-package com.api.controllers.employee;public class EmployeePickupPointController {
+package com.api.controllers.employee;
+
+import com.api.services.pickup_point.PickupPointService;
+import com.shared.db.entities.Account;
+import com.shared.response.CommonResponse;
+import com.shared.utils.DateConvertUtil;
+import com.shared.utils.ResponseUtil;
+import java.time.Instant;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/employee/pickup-point")
+@RequiredArgsConstructor
+@Slf4j
+public class EmployeePickupPointController {
+    private final PickupPointService pickupPointService;
+
+    @GetMapping("/list-ride-id")
+    @PreAuthorize("hasAnyAuthority('EMPLOYEE', 'ADMIN')")
+    public ResponseEntity<CommonResponse<Object>> getListRideId(
+            @AuthenticationPrincipal Account account,
+            String date
+    ) {
+        return ResponseUtil.toSuccessCommonResponse(
+                pickupPointService.getListRideId(account, DateConvertUtil.convertStringToInstant(date))
+        );
+    }
+
+    @GetMapping("/manipulate")
+    @PreAuthorize("hasAnyAuthority('EMPLOYEE', 'ADMIN')")
+    public ResponseEntity<CommonResponse<Object>> getListManipulatePickupPoint(
+            @AuthenticationPrincipal Account account,
+            String date,
+            Long rideId
+    ) {
+        return ResponseUtil.toSuccessCommonResponse(
+                pickupPointService.getListManipulatePickupPoint(account,
+                        DateConvertUtil.convertStringToInstant(date), rideId)
+        );
+    }
 }
