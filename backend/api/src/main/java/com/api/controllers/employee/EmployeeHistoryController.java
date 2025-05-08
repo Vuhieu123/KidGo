d1b2bd2e3d3f496eff4ad06fb2edd2f3ec0e4d60
Vu Hieu <hieutrongvu281@gmail.com>
2025-05-08 23:59:00 +0700

    feat(api): update employee controller

---
+++ backend/api/src/main/java/com/api/controllers/employee/EmployeeHistoryController.java
@@ -0,0 +1,43 @@
+package com.api.controllers.employee;
+
+import com.api.services.history.HistoryService;
+import com.api.services.history.dto.EmployeeHistoryRideFilterParam;
+import com.shared.db.entities.Account;
+import com.shared.response.CommonResponse;
+import com.shared.utils.PageableUtils;
+import com.shared.utils.ResponseUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/employee/history")
+@RequiredArgsConstructor
+@Slf4j
+public class EmployeeHistoryController {
+    private final HistoryService historyService;
+
+    @GetMapping("/ride/pagination")
+    @PreAuthorize("hasAnyAuthority('EMPLOYEE', 'ADMIN')")
+    public ResponseEntity<CommonResponse<Object>> getAdminHistoryRides(
+            EmployeeHistoryRideFilterParam filterParam,
+            @AuthenticationPrincipal Account account
+    ) {
+        Pageable pageable = PageableUtils.generate(
+                filterParam.getPage(),
+                filterParam.getSize(),
+                filterParam.getSort(),
+                "-id"
+        );
+
+        return ResponseUtil.toSuccessCommonResponse(
+                historyService.getEmployeeHistoryRides(filterParam, pageable, account)
+        );
+    }
+}
---
+++ backend/api/src/main/java/com/api/controllers/employee/EmployeeStudentPickupPointController.java
@@ -0,0 +1,34 @@
+package com.api.controllers.employee;
+
+import com.api.controllers.employee.dto.UpdateStudentPickupPointRequest;
+import com.api.services.student_pickup_point.StudentPickupPointService;
+import com.shared.db.entities.Account;
+import com.shared.response.CommonResponse;
+import com.shared.utils.ResponseUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/employee/student-pickup-point")
+@RequiredArgsConstructor
+@Slf4j
+public class EmployeeStudentPickupPointController {
+    private final StudentPickupPointService studentPickupPointService;
+
+    @PutMapping()
+    @PreAuthorize("hasAnyAuthority('EMPLOYEE', 'ADMIN')")
+    public ResponseEntity<CommonResponse<Object>> updateStudentPickupPoints(
+            @RequestBody UpdateStudentPickupPointRequest request,
+            @AuthenticationPrincipal Account account
+    ) {
+        studentPickupPointService.updateStudentPickupPointEmployee(request.toInput(), account);
+        return ResponseUtil.toSuccessCommonResponse("Update student pickup point successfully");
+    }
+}
