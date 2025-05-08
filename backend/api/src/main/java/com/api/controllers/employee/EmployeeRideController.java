22fd32f66568645ec87dca71f60e11d1b46bce8d
Vu Hieu <hieutrongvu281@gmail.com>
2025-05-08 23:57:04 +0700

    feat(api): create employee ride api

---
+++ backend/api/src/main/java/com/api/controllers/employee/EmployeeRideController.java
@@ -0,0 +1,34 @@
+package com.api.controllers.employee;
+
+import com.api.controllers.employee.dto.UpdateRideRequest;
+import com.api.services.ride.RideService;
+import com.shared.db.entities.Account;
+import com.shared.response.CommonResponse;
+import com.shared.utils.ResponseUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/employee/ride")
+@RequiredArgsConstructor
+@Slf4j
+public class EmployeeRideController {
+    private final RideService rideService;
+
+    @PutMapping()
+    @PreAuthorize("hasAnyAuthority('EMPLOYEE', 'ADMIN')")
+    public ResponseEntity<CommonResponse<Object>> updateRide(
+            @RequestBody UpdateRideRequest request,
+            @AuthenticationPrincipal Account account
+    ) {
+        rideService.updateRideEmployee(request.toInput(), account);
+        return ResponseUtil.toSuccessCommonResponse("Update bus successfully");
+    }
+}
